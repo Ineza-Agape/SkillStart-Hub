c56e451b390008490d470d09621542f83453e97a
Ineza-Agape <i.agape@alustudent.com>
2025-03-06 15:45:29 +0200

    centered login

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 document.addEventListener("click", function () {
-    let heroText = document.querySelector(".hero-text");
     let loginSection = document.getElementById("loginSection");
 
-    // Fade out the hero text
-    heroText.classList.add("fade-out");
-
-    // After a delay, show the login section and slide it up
-    setTimeout(() => {
-        loginSection.classList.add("show");
-    }, 500); // Delay to ensure the hero text fades out first
+    loginSection.style.opacity = 1;
+    loginSection.style.top = "0";
 });

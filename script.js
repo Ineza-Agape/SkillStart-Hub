6d64fbf540f95b736b9862ca35f20425726758c6
Ineza-Agape <i.agape@alustudent.com>
2025-03-06 15:28:10 +0200

    login section2

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("click", function () {
     let heroText = document.querySelector(".hero-text");
     let loginSection = document.getElementById("loginSection");
 
+    // Fade out the hero text
     heroText.classList.add("fade-out");
 
+    // After a delay, show the login section and slide it up
     setTimeout(() => {
         loginSection.classList.add("show");
-    }, 500);
+    }, 500); // Delay to ensure the hero text fades out first
 });

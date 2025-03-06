accdf32d4e83406928982486eab6656ae74448a0
Ineza-Agape <i.agape@alustudent.com>
2025-03-06 18:00:32 +0200

    scroll up

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ document.addEventListener("click", function () {
     loginSection.style.opacity = 1; // Make the login section visible
     loginSection.style.top = "0";   // Slide it up to the top
 });
+
+window.addEventListener('scroll', function() {
+    const content = document.querySelector('.home-content');
+    if (window.scrollY > 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});

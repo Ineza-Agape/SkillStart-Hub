7b82bed7ca37a0c07fbb4c75f3103e9317ed830e
Ineza-Agape <i.agape@alustudent.com>
2025-03-07 10:02:22 +0200

    commentary

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener("click", function () {
     let loginSection = document.getElementById("loginSection");
 
-    // Make the login section visible and slide it up to the center
-    loginSection.style.opacity = 1; // Make the login section visible
-    loginSection.style.top = "0";   // Slide it up to the top
+    loginSection.style.opacity = 1;
+    loginSection.style.top = "0";
 });
 
 window.addEventListener('scroll', function() {
